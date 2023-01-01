701b41d8ebf94aa4be6fa625c0d992c35002f394
YHKwon <ssugy2@gmail.com>
2023-01-01 17:41:08 +0900

    기본 gitignore 세팅

---
+++ Language/CSharp/BasicProject/BasicProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
