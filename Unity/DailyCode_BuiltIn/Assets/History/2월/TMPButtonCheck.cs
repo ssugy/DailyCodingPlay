9e0cb6402de388ec2989ba8491f43871d56f1ed3
DevYH <ssugy2@gmail.com>
2023-02-09 21:10:29 +0900

    파일 정리

---
+++ Unity/DailyCode_BuiltIn/Assets/History/2월/TMPButtonCheck.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TMPButtonCheck : MonoBehaviour
+{
+    public TextMeshProUGUI button;
+    public TextMeshPro test;
+
+    public Button btn;
+}
