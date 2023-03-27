6ff142da04d75d6222c7682c2e11c809471e8c06
DevYH <ssugy2@gmail.com>
2023-03-27 23:21:36 +0900

    WOL ORTCP연동코드 작성 중

---
+++ Unity/DailyCode_BuiltIn/Assets/History/3월_4_WOL/StringToByteArr.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StringToByteArr : MonoBehaviour
+{
+    public byte[] m_MacAddress;
+    public string ipAddress = "B0-6E-BF-CE-77-04";    // 00-00-00-00-00-03
+
+    private void Start()
+    {
+        //m_MacAddress = Encoding.;
+    }
+}
