b7a1e91525088cf1675e6b6f86ff41fb9dc8a71a
DevYH <ssugy2@gmail.com>
2023-03-29 22:34:09 +0900

    폴더여는부분 스터디. 파일이름 가져오는 방법 구현

---
+++ Unity/DailyCode_BuiltIn/Assets/History/3월_12_폴더 열기_Avpro/FolderCheck.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using RenderHeads.Media.AVProVideo;
+using SimpleFileBrowser;
+using DpTool;
+
+public class FolderCheck : MonoBehaviour
+{
+    public Button fileOpenBtn;
+    public MediaPlayer mediaPlayer;
+    public RawImage preview;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fileOpenBtn.onClick.AddListener(FileOpen);
+    }
+
+    private void FileOpen()
+    {
+        //string path = $"D:\\tmp\\test.txt"; // 파일찍으면 바로 실행
+        //Process proc = Process.Start(path);  // 실행의 용도
+
+        FileBrowser.ShowLoadDialog(
+            (success) =>
+            {
+                if (success.Length != 0)
+                {
+                    UnityEngine.Debug.Log(success[0]);  // 절대 경로는 가져올 수 있다.
+                    mediaPlayer.OpenVideoFromFile(MediaPlayer.FileLocation.AbsolutePathOrURL, success[0], false);   // 여기서 잘못된 파일을 가져오면,[AVProVideo] Error: Loading failed. 에러가 뜬다.
+                    mediaPlayer.Play();
+                }
+            },
+            () => { },
+            FileBrowser.PickMode.Files);
+    }
+
+    // 한번만 적용되면 플레이 화면이 그대로 계속 나온다.
+    public void SetPreviewTexture()
+    {
+        Debug.Log("이벤트 시작");
+        preview.texture = mediaPlayer.TextureProducer.GetTexture();
+    }
+
+
+}
