d288324fadc8a518977c1db0912d94ae606e0120
DAS_MSI\YHKwon <ssugy2@gmail.com>
2023-08-25 22:45:00 +0900

    cmd작업 진행

--- Language/CSharp/BasicProject/02. Time/TimeMain.cs
+++ Language/CSharp/BasicProject/02. Time/TimeMain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,28 @@ namespace BasicProject._02._Time
 {
     internal class TimeMain
     {
-        public static void _Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine($"{DateTime.Now.ToString("yyyy MM dd"+"/"+"dd")}");
             Console.WriteLine($"{DateTime.Now.ToString("//")}");
+            
+            Console.WriteLine($"{DateTime.Now.DayOfWeek}");
+            Console.WriteLine("---");
+            DateTimeFormatInfo dtfi = new DateTimeFormatInfo();
+            foreach (var item in dtfi.DayNames)
+            {
+                Console.WriteLine(item.Substring(0, 3));
+            }
+            Console.WriteLine("---");
+            Console.WriteLine(dtfi.DayNames[0]);
+            Console.WriteLine($"{DateTime.Now}");
+            Console.WriteLine($"{DateTime.Now.AddDays(30).ToString("yyyy/MM/dd")}");
+            Console.WriteLine("---");
+
+            Console.WriteLine((int)Enum.Parse(typeof(DayOfWeek), "Monday"));
+
+            // 특정 요일을 주어졌을 때, 해당 요일에 해당하는 날짜 구하기
+
             Console.ReadLine();
         }
     }
--- Unity/DailyCode_BuiltIn/Assets/History/23년 7월/06. cmd 실행/CMD.cs
+++ Unity/DailyCode_BuiltIn/Assets/History/23년 7월/06. cmd 실행/CMD.cs
@@ -1,3 +1,5 @@
+using AForge.Genetic;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,6 +12,17 @@ public enum Schedule
     Daily,          // 1일단위 반복
     Weekly,         // 주단위 반복
     Once
+}
+
+public enum DayOfWeekShort
+{
+    Sun = 0,
+    Mon,
+    Tue,
+    Wed,
+    Thu,
+    Fri,
+    Sat,
 }
 
 public class CMD : MonoBehaviour
@@ -41,7 +54,7 @@ public class CMD : MonoBehaviour
         string restartCMD = "shutdown -s -f -t 0";
         ExcuteCMD(restartCMD);
     }
-    
+
     // PC Restart 실행
     public static void PCRestart()
     {
@@ -58,5 +71,51 @@ public class CMD : MonoBehaviour
     * ed : end date (종료 날짜, once에서는 하면안됨.)
     * F : 지정된 작업이 존재하는 경우 작업을 강제로 만듬
     */
-    
+    public static void MakeScheduler(Schedule type, string OffTime, string day)
+    {
+        string query = string.Empty;
+        switch (type)
+        {
+            case Schedule.Disable:
+                // 삭제는 1개씩 밖에 안된다.
+                query = $"chcp 65001 && schtasks /delete /tn \"CMS\" /F";
+                break;
+            case Schedule.Daily:
+                query = $"chcp 65001 && schtasks /create /tn \"CMS\" /tr \"C:\\Windows\\System32\\shutdown.exe  -s -f -t 0\" /sc daily /st {OffTime} /F";
+                break;
+            case Schedule.Weekly:
+                query = $"chcp 65001 && schtasks /create /tn \"CMS\" /tr \"C:\\Windows\\System32\\shutdown.exe  -s -f -t 0\" /sc weekly /d {day} /st {OffTime} /F";
+                break;
+            case Schedule.Once:
+                // once는 특정 날짜를 기준으로 해야하는데, 특정 요일로 되어있음.
+                // 요일을 보고 그 차이만큼 날짜를 더해야 한다.
+                UnityEngine.Debug.Log((int)Enum.Parse(typeof(DayOfWeekShort), day));
+                int today = (int)DateTime.Now.DayOfWeek;
+                int OnceDay = (int)Enum.Parse(typeof(DayOfWeekShort), day); // 이게 무조건 뒤에 있는 개념이다.
+                int offset = (OnceDay - today) < 0 ? OnceDay - today + 7 : OnceDay - today;
+                string onceDate = DateTime.Now.AddDays(offset).ToString("yyyy/MM/dd");
+
+                query = $"chcp 65001 && schtasks /create /tn \"CMS_Once\" /tr \"C:\\Windows\\System32\\shutdown.exe  -s -f -t 0\" /sc once /sd {onceDate} /st {OffTime} /F";
+                break;
+        }
+
+        ExcuteCMD(query);
+    }
+
+    // 테스트용
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CMD.MakeScheduler(Schedule.Disable, "10:12", "Mon");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            CMD.MakeScheduler(Schedule.Daily, "10:12", "Mon");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            CMD.MakeScheduler(Schedule.Once, "14:12", "Fri");
+        }
+    }
 }
